d3208d732c88114204a528cc4c9add9d7890e8a0
wangyoucao577 <wangyoucao577@gmail.com>
2018-02-13 01:59:30 -0800

    test for pkg and test framework

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,12 @@
 
 package main
 
-import  "fmt"
+import (
+    "fmt"
+    "github.com/wangyoucao577/golang_test/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello world\n");
+    fmt.Printf(stringutil.Reverse("hello world\n"));
 }
 

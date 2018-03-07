37544f6161c6ddca716d1df65ee99f7c64488a82
wangyoucao577 <wangyoucao577@gmail.com>
2018-03-06 22:47:14 -0800

    format by gofmt

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,10 @@
-
 package main
 
 import (
-    "fmt"
-    "github.com/wangyoucao577/golang_test/stringutil"
+	"fmt"
+	"github.com/wangyoucao577/golang_test/stringutil"
 )
 
 func main() {
-    fmt.Printf(stringutil.Reverse("hello world\n"));
+	fmt.Printf(stringutil.Reverse("hello world\n"))
 }
-

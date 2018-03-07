698c1ad42d4758bdbd39dc92e95a6f1aaef2b824
wangyoucao577 <wangyoucao577@gmail.com>
2018-03-06 23:58:26 -0800

    go sample from gopl ch1 echo3

---
+++ my_gopl_test/ch1_echo3/echo3.go
@@ -0,0 +1,15 @@
+// prints its command-line arguments
+package main
+
+import(
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main(){
+    //fmt.Println(os.Args[1:])
+    fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+

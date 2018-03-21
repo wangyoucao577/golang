78a61995cd6630880941b6e520bce498bc3db19a
wangyoucao577 <wangyoucao577@gmail.com>
2018-03-21 00:22:39 -0700

    go sample from gopl ch1.7 server2

---
+++ my_gopl_test/ch1_server2/main.go
@@ -0,0 +1,33 @@
+// Server1 is a minimal "echo" server. 
+package main 
+
+import (
+"fmt"
+"log"
+"net/http"
+"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+    http.HandleFunc("/", handler) // each request calls handler
+    http.HandleFunc("/count", counter)
+    log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+}
+
+// handler echoes the Path component of the request URL r. 
+func handler(w http.ResponseWriter, r *http.Request){
+    mu.Lock()
+    count++
+    mu.Unlock()
+    fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+}
+
+// counter echoes the number of calls so far. 
+func counter(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    fmt.Fprintf(w, "Count %d\n", count)
+    mu.Unlock()
+}

fd298571485091afdb02cf4c788ad256313b4278
wangyoucao577 <wangyoucao577@gmail.com>
2018-03-21 01:46:49 -0700

    go sample from gopl ch1 exercise 1.12, change cycles by query string param

--- my_gopl_test/ch1_12_server_lissajous/main.go
+++ my_gopl_test/ch1_12_server_lissajous/main.go
@@ -1,50 +1,61 @@
-// Server1 is a minimal "echo" server. 
-package main 
+// Server1 is a minimal "echo" server.
+package main
 
 import (
-"fmt"
-"log"
-"net/http"
-"sync"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
 )
 
 var mu sync.Mutex
 var count int
 
 func main() {
-    http.HandleFunc("/", handler) // each request calls handler
-    http.HandleFunc("/count", counter)
-    http.HandleFunc("/lissajous", func(w http.ResponseWriter, r *http.Request){
-        lissajous(w)
-        })
-    log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+	http.HandleFunc("/", handler) // each request calls handler
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/lissajous/", func(w http.ResponseWriter, r *http.Request) {
+
+		cycles_value_str := r.URL.Query().Get("cycles")
+		if cycles_value_str != "" {
+			if cycles_value, err := strconv.ParseFloat(cycles_value_str, 64); err != nil {
+				lissajous(w, cycles_value)
+				return
+			}
+		}
+		lissajous(w, 5)
+
+	})
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
 }
 
-// handler echoes the http request. 
-func handler(w http.ResponseWriter, r *http.Request){
-    mu.Lock()
-    count++
-    mu.Unlock()
-    //fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+// handler echoes the http request.
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	//fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
 
-    fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-    for k, v := range r.Header {
-        fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-    }
-    fmt.Fprintf(w, "Host = %q\n", r.Host)
-    fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-    if err := r.ParseForm(); err != nil {
-        log.Print(err)
-    }
-    for k, v := range r.Form {
-        fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-    }
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 
 }
 
-// counter echoes the number of calls so far. 
+// counter echoes the number of calls so far.
 func counter(w http.ResponseWriter, r *http.Request) {
-    mu.Lock()
-    fmt.Fprintf(w, "Count %d\n", count)
-    mu.Unlock()
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
 }

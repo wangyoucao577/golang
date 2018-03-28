fab0ab93d4aea2117c73b5e4827afa0b37be6f6e
wangyoucao577 <wangyoucao577@gmail.com>
2018-03-28 02:34:06 -0700

    go sample from gopl ch2.6: another way for complex init

--- my_gopl_test/ch2_popcount/popcount.go
+++ my_gopl_test/ch2_popcount/popcount.go
@@ -1,13 +1,18 @@
 package popcount
 
 // pc[i] is the population count of i.
-var pc [256]byte
-
-func init() {
+var pc [256]byte = func() (pc [256]byte) {
 	for i := range pc {
 		pc[i] = pc[i/2] + byte(i&1)
 	}
-}
+	return
+}()
+
+// func init() {
+// 	for i := range pc {
+// 		pc[i] = pc[i/2] + byte(i&1)
+// 	}
+// }
 
 // PopCount returns the population count (number of set bits) of x.
 func PopCount(x uint64) int {

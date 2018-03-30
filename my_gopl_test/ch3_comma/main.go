3042dec695de3230c05be60c5999eb547d2f1a1b
wangyoucao577 <wangyoucao577@gmail.com>
2018-03-30 07:48:15 -0700

    go sample from gopl ch3.5

---
+++ my_gopl_test/ch3_comma/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(comma("12"))
+	fmt.Println(comma("123"))
+	fmt.Println(comma("1234"))
+	fmt.Println(comma("12345"))
+	fmt.Println(comma("123456"))
+	fmt.Println(comma("1234567"))
+}
+
+//comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}

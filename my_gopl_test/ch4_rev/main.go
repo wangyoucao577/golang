b8372962fdca33e47e739e726d36d1579352c927
wangyoucao577 <wangyoucao577@gmail.com>
2018-05-02 01:07:20 -0700

    use reverse to try rotate

--- my_gopl_test/ch4_rev/main.go
+++ my_gopl_test/ch4_rev/main.go
@@ -11,8 +11,21 @@ func reverse(s []int) {
 	}
 }
 
+// Rotate s left by num positions
+func rotate(s []int, num int) {
+	reverse(s[:num])
+	reverse(s[num:])
+	reverse(s)
+	//fmt.Println(s)
+}
+
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Println(a)
+
 	reverse(a[:])
+	fmt.Println(a)
+
+	rotate(a[:], 2)
 	fmt.Println(a)
 }

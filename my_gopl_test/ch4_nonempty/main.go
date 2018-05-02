709c7b3465cca98c6e065aff55c0273dce8f5b1d
wangyoucao577 <wangyoucao577@gmail.com>
2018-05-02 02:30:50 -0700

    go sample from gopl ch4 nonempty'

---
+++ my_gopl_test/ch4_nonempty/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// nonempty returns a slice holding only the non-empty strings.
+// The underlying array is modified during the call .
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
+
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data)) // `["one", "three"]`
+	fmt.Printf("%q\n", data)           // `["one", "three", "three"]`
+}

dadd9c29e285cf7acf9f7a574fd9720c17d38a32
wangyoucao577 <wangyoucao577@gmail.com>
2018-05-02 03:12:52 -0700

    go sample from gopl ch4 exercist 4.3

---
+++ my_gopl_test/ch4_3_rev/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// reverse reverses a slice of ints in place.
+func reverse(s *[6]int) {
+	for i, j := 0, len(*s)-1; i < j; i, j = i+1, j-1 {
+		(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
+	}
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Println(a)
+
+	reverse(&a)
+	fmt.Println(a)
+
+}

b5707b97e3414a4f47bc2c4553d66f1395b5d85d
wangyoucao577 <wangyoucao577@gmail.com>
2018-05-11 07:20:14 -0700

    go sample from gopl ch4.5 search github issues

---
+++ my_gopl_test/ch4_github_issues/main.go
@@ -0,0 +1,20 @@
+// Issues prints a table of Github issues matching the search terms.
+package main
+
+import (
+	"fmt"
+	"github.com/wangyoucao577/golang_test/my_gopl_test/ch4_github"
+	"log"
+	"os"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}

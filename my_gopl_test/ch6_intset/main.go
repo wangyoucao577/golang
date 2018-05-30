970d8e9a2a6f8cf4dc5f17e72844b3752dfc7876
wangyoucao577 <wangyoucao577@gmail.com>
2018-05-29 19:22:30 -0700

    go sample from gopl ch6.5 exercise 6.1

--- my_gopl_test/ch6_intset/main.go
+++ my_gopl_test/ch6_intset/main.go
@@ -26,6 +26,36 @@ func (s *IntSet) Add(x int) {
 	s.words[word] |= 1 << bit
 }
 
+// Remove remove x from the set
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &= ^(1 << bit)
+}
+
+// Clear remove all elements from the set
+func (s *IntSet) Clear() {
+	s.words = s.words[:0]
+}
+
+// Len return the number of elements
+func (s *IntSet) Len() int {
+	var len int
+	for _, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				len++
+			}
+		}
+	}
+	return len
+}
+
 // UnionWith sets s to the union of s and t.
 func (s *IntSet) UnionWith(t *IntSet) {
 	for i, tword := range t.words {
@@ -35,6 +65,15 @@ func (s *IntSet) UnionWith(t *IntSet) {
 			s.words = append(s.words, tword)
 		}
 	}
+}
+
+// Copy return a copy of the set
+func (s *IntSet) Copy() *IntSet {
+	n := new(IntSet)
+	for _, word := range s.words {
+		n.words = append(n.words, word)
+	}
+	return n
 }
 
 // String returns the set as a string of te form "{1 2 3}"
@@ -64,16 +103,39 @@ func main() {
 	x.Add(144)
 	x.Add(9)
 	fmt.Println(x.String()) // "{1 9 144}"
+	fmt.Println(x.Len())    // 3
 
 	y.Add(9)
 	y.Add(42)
 	fmt.Println(y.String()) // "{9 42}"
+	fmt.Println(y.Len())    // 2
 
 	x.UnionWith(&y)
 	fmt.Println(x.String())           // "{1 9 42 144}"
+	fmt.Println(x.Len())              // 4
 	fmt.Println(x.Has(9), x.Has(123)) // "true false"
 
 	fmt.Println(&x)         // "{1 9 42 144}"
 	fmt.Println(x.String()) // "{1 9 42 144}"
 	fmt.Println(x)
+
+	z := x.Copy()
+	fmt.Println(z) // "{1 9 42 144}"
+
+	x.Remove(2)
+	fmt.Println(&x) // "{1 9 42 144}"
+	x.Remove(42)
+	fmt.Println(&x) // "{1 9 144}"
+	x.Remove(145)
+	fmt.Println(&x) // "{1 9 144}"
+	x.Remove(144)
+	fmt.Println(&x) // "{1 9}"
+
+	fmt.Println(x.Len()) // 2
+	fmt.Println(&x)      // "{1 9}"
+	x.Clear()
+	fmt.Println(x.Len()) // 0
+	fmt.Println(&x)      // "{}"
+
+	fmt.Println(z) // "{1 9 42 144}"
 }

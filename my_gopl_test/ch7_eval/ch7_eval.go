baf0d706b6676ad4f577bc3004eb230c43acb363
wangyoucao577 <wangyoucao577@gmail.com>
2018-07-04 02:02:34 -0700

    add check interface

--- my_gopl_test/ch7_eval/ch7_eval.go
+++ my_gopl_test/ch7_eval/ch7_eval.go
@@ -3,11 +3,15 @@ package ch7_eval
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 // An Expr is an arithmetic expression.
 type Expr interface {
 	Eval(env Env) float64
+
+	// Check reports errors in this Expr and adds its Vars to the set.
+	Check(vars map[Var]bool) error
 }
 
 // A Var identifies a variable, e.g., x
@@ -79,3 +83,48 @@ func (c call) Eval(env Env) float64 {
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
+
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d",
+			c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}

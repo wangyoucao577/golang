9eddbec3cc909fc73a6a9494faeccaa8bf758782
wangyoucao577 <wangyoucao577@gmail.com>
2018-07-28 02:38:19 -0700

    go sample from gopl ch8.4.2 pipeline1

---
+++ my_gopl_test/ch8_pipeline1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	//Counter
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+			time.Sleep(1 * time.Second) // count one per second
+		}
+	}()
+
+	// Squarer
+	go func() {
+		for {
+			x := <-naturals
+			squares <- x * x
+		}
+	}()
+
+	// Printer in main goroutine
+	for {
+		fmt.Println(<-squares)
+	}
+
+}

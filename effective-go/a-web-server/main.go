9eec843f9518a5e20423a3b2333db1af6802399e
Jay Zhang <wangyoucao577@gmail.com>
2020-02-08 15:18:38 +0800

    feat: a web server sample code from effective go

---
+++ effective-go/a-web-server/main.go
@@ -0,0 +1,47 @@
+// Package main comes from "Effective Go - A web server" sample code,
+// use google's chart api to generate QR from contents.
+// https://golang.org/doc/effective_go.html#web_server
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
+
+var templ = template.Must(template.New("qr").Parse(templateStr))
+
+func main() {
+	flag.Parse()
+
+	http.Handle("/", http.HandlerFunc(qr))
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func qr(w http.ResponseWriter, req *http.Request) {
+	templ.Execute(w, req.FormValue("s"))
+}
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET"><input maxLength=1024 size=70
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
